8c149f49459d37ae99be373506dc733c78dc122d
bonganimasemola <bonganimasemola@icloud.com>
2024-01-15 12:34:14 +0300

    Database models and validations set up

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -3,10 +3,19 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Hero
+from models import db,Power
+
+import os
+
+cwd=os.getcwd()
+
+db_dir=f'sqlite:////{cwd}/db/app.db'
+
+print(db_dir)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/bonganimasemola/Development/coding/PHASE4/python-code-challenge-superheroes/code-challenge/app/db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =db_dir
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
